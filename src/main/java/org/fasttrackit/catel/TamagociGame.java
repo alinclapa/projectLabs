e2c3623cb07b25068ba18e23f440034ed9c561d6
alinclapa <alin.clapa@yahoo.com>
2019-03-11 21:28:43 +0200

    starting tamagoci game

---
+++ src/main/java/org/fasttrackit/catel/TamagociGame.java
@@ -0,0 +1,23 @@
+package org.fasttrackit.catel;
+
+import java.sql.SQLOutput;
+
+public class TamagociGame {
+
+    public static void main(String[] args) {
+
+        Catel dog1 = new Catel();
+        dog1.setName("Rex");
+
+
+        Catel dog2 = new Catel();
+        dog2.setName("Iisus");
+
+        for (int zi=0;zi<15;zi++) {
+            System.out.println(dog2.getVarsta());
+            dog2.mananca();
+        }
+
+        System.out.println("Catelul " + dog2.getName() + "are varsta :" + dog2.getVarsta());
+    }
+}
